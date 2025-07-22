73d4ad38fa0b1b7e683bd7d2d6c3619cdc6450e9
parth448 <101268319+parth448@users.noreply.github.com>
2025-07-22 17:03:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-# Fetch default VPC
+# Get default VPC
 data "aws_vpc" "default" {
   default = true
 }
 
-# Fetch default subnets in the default VPC
+# Get default subnets in the default VPC
 data "aws_subnets" "default" {
   filter {
     name   = "vpc-id"
@@ -15,15 +15,15 @@ data "aws_subnets" "default" {
   }
 }
 
-# Use first default subnet
+# Use one default subnet for EC2 instances
 data "aws_subnet" "default_subnet" {
   id = data.aws_subnets.default.ids[0]
 }
 
-# Create a Security Group for ALB and EC2s
+# Create a Security Group (renamed to avoid duplication)
 resource "aws_security_group" "web_sg" {
-  name        = "web-sg"
-  description = "Allow HTTP access"
+  name        = "web-sg-demo"
+  description = "Allow HTTP traffic"
   vpc_id      = data.aws_vpc.default.id
 
   ingress {
@@ -41,15 +41,15 @@ resource "aws_security_group" "web_sg" {
   }
 
   tags = {
-    Name = "web-sg"
+    Name = "web-sg-demo"
   }
 }
 
-# EC2 Instance 1
+# Launch EC2 instance 1
 resource "aws_instance" "web1" {
-  ami                    = "ami-0b32d400456908bf9"
-  instance_type          = "t3.micro"
-  subnet_id              = data.aws_subnet.default_subnet.id
+  ami           = "ami-0b32d400456908bf9"
+  instance_type = "t3.micro"
+  subnet_id     = data.aws_subnet.default_subnet.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
 
@@ -67,11 +67,11 @@ resource "aws_instance" "web1" {
   }
 }
 
-# EC2 Instance 2
+# Launch EC2 instance 2
 resource "aws_instance" "web2" {
-  ami                    = "ami-0b32d400456908bf9"
-  instance_type          = "t3.micro"
-  subnet_id              = data.aws_subnet.default_subnet.id
+  ami           = "ami-0b32d400456908bf9"
+  instance_type = "t3.micro"
+  subnet_id     = data.aws_subnet.default_subnet.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
 
@@ -89,8 +89,30 @@ resource "aws_instance" "web2" {
   }
 }
 
-# Application Load Balancer
-resource "aws_lb" "web_alb" {
+# Create Target Group
+resource "aws_lb_target_group" "tg" {
+  name        = "web-target-group"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = data.aws_vpc.default.id
+  target_type = "instance"
+}
+
+# Attach EC2s to Target Group
+resource "aws_lb_target_group_attachment" "web1" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.web1.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "web2" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.web2.id
+  port             = 80
+}
+
+# Create Application Load Balancer
+resource "aws_lb" "alb" {
   name               = "web-alb"
   internal           = false
   load_balancer_type = "application"
@@ -102,36 +124,14 @@ resource "aws_lb" "web_alb" {
   }
 }
 
-# Target Group for ALB
-resource "aws_lb_target_group" "web_tg" {
-  name        = "web-tg"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = data.aws_vpc.default.id
-  target_type = "instance"
-}
-
-# Attach EC2 Instances to Target Group
-resource "aws_lb_target_group_attachment" "attach_web1" {
-  target_group_arn = aws_lb_target_group.web_tg.arn
-  target_id        = aws_instance.web1.id
-  port             = 80
-}
-
-resource "aws_lb_target_group_attachment" "attach_web2" {
-  target_group_arn = aws_lb_target_group.web_tg.arn
-  target_id        = aws_instance.web2.id
-  port             = 80
-}
-
-# ALB Listener
-resource "aws_lb_listener" "web_listener" {
-  load_balancer_arn = aws_lb.web_alb.arn
+# Create Listener
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.alb.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.web_tg.arn
+    target_group_arn = aws_lb_target_group.tg.arn
   }
 }
